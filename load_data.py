9507cb2d6135cbced5bcdb3887994c9142d2ef35
vinsky001 <ekibet544@gmail.com>
2025-01-22 14:28:57 +0300

    updated the code to use the load_data function

--- load_data.py
+++ load_data.py
@@ -10,14 +10,15 @@ def load_data():
     Open mnist dataset and laods the data in form of list.
     combines the training and testing into one
     """
-    data = deep-learning-hello-world(getcwd() + 'mnist_data')
-    train_images, train_labels = data.load_traing()
+    data = MNIST(getcwd() + '/mnist_data')
+    #data.gz = True
+    train_images, train_labels = data.load_training()
     test_images, test_labels = data.load_testing()
     images = train_images + test_images
     labels = list(train_labels) + list(test_labels)
     return images, labels
 
-images, labels = get_mnist_data()
+images, labels = load_data()
 
 #Prepare the outputs(labels) for a classification problem:
 
